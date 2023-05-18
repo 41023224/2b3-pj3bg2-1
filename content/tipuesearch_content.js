0f2267614623f2a7e2d7d6f7fa18c2c640e61b4b
41023217 <jerry923460@gmail.com>
2023-05-18 14:30:49 +0800

    41023217

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組長 :\xa0 \n 組員:41023240 葉桓亞 \n 組員:41023241 詹耀賢 \n github repository \n github page \n \n \n \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組長 : 41023217 陳育瑋 \n 組員:41023240 葉桓亞 \n 組員:41023241 詹耀賢 \n github repository \n github page \n \n \n \n \n', 'tags': '', 'url': 'About.html'}]};
