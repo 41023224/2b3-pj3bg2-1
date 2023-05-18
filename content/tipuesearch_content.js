22ac06ca5ea2ed18773b59b1ac6661db5fc32c52
YHY30678 <yhywtf30678@gmail.com>
2023-05-18 16:03:44 +0800

    group

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組長 : 41023217 陳育瑋 \n 組員:41023201 張若芹 \n 組員:41023215 許嘉祐 \n 組員:41023240 葉桓亞 \n 組員:41023241 詹耀賢 \n github repository \n github page \n \n \n \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組長 : 41023217 陳育瑋 \n 組員:41023201 張若芹 \n 組員:41023215 許嘉祐 \n 組員:41023240 葉桓亞 \n 組員:41023241 詹耀賢 \n github repository \n github page \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'group', 'text': '\n \n \n', 'tags': '', 'url': 'group.html'}]};
